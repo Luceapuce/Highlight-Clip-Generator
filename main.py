5a0cc93e4aa7514fcda7a0a0669ff6b7e6409dfa
Lucy Ivatt <li571@york.ac.uk>
2019-08-11 19:38:28 +0100

    Update main.py
    Added a feature which allows clipping of multiple games using a numbered menu to choose the directory required.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from os import rename
 from multiprocessing import Process
 
 # Directory Constants
-ORIGINAL_DIRECTORY = "D:\\Users\\lucea\\Videos\\Hunt  Showdown\\"
-HIGHLIGHT_DIRECTORY = "D:\\Users\\lucea\\Videos\\Hunt  Showdown\\Highlights\\"
+BASE_DIRECTORY = "D:\\Users\\lucea\\Videos\\"
+GAME_LIST = ["Counter-strike  Global Offensive", "Hunt Showdown", "Rocket League"]
 
 # Converts time strings in the format "Minutes Seconds" to required tuple (minutes, seconds)
 def string_to_tuple(string):
@@ -14,7 +14,7 @@ def string_to_tuple(string):
         int_list.append(int(element))
     return tuple(int_list)
 
-def clip_video(original_path, start_time, end_time, highlight_name):
+def clip_video(original_path, start_time, end_time, highlight_name, chosen_game):
     # Accesses raw video footage
     original_file = VideoFileClip(original_path)
 
@@ -26,8 +26,8 @@ def clip_video(original_path, start_time, end_time, highlight_name):
     highlight_file = original_file.subclip(start_time_tuple, end_time_tuple)
 
     # Creates file path and exports video to highlights folder
-    highlight_path = HIGHLIGHT_DIRECTORY + highlight_name + ".mp4"
-    highlight_file.write_videofile(highlight_path, temp_audiofile="D:\\Users\\lucea\\Videos\\Hunt  Showdown\\random_name.mp3")
+    highlight_path = BASE_DIRECTORY + GAME_LIST[chosen_game] + "\\" + "Highlights" + "\\" + highlight_name + ".mp4"
+    highlight_file.write_videofile(highlight_path, temp_audiofile="D:\\Users\\lucea\\Videos\\random_name.mp3")
 
     # Closes both files
     original_file.reader.close()
@@ -37,22 +37,27 @@ def clip_video(original_path, start_time, end_time, highlight_name):
 
 if __name__ == "__main__":
 
+    # Game Selection
+    print("List of Games Available:\n")
+    for i, item in enumerate(GAME_LIST,1):
+        print(i, '. ' + item + "\n")
+    chosen_game = int(input("Enter the number required: ")) - 1
+
+    # Data Input
+    original_name = input("Original name of video: ")
+    start_time = input("Start time [minutes seconds]: ")
+    end_time = input("End time [minutes seconds]: ")
+    highlight_name = input("Name of highlight: ")
+
+    # Adds the file name to the path
+    original_path = BASE_DIRECTORY + GAME_LIST[chosen_game] + "\\" + original_name + ".mp4"
+
     try:
-        # Data Input
-        original_name = input("Original name of video: ")
-        start_time = input("Start time [minutes seconds]: ")
-        end_time = input("End time [minutes seconds]: ")
-        highlight_name = input("Name of highlight: ")
-
-        # Adds the file name to the path
-        original_path = ORIGINAL_DIRECTORY + original_name + ".mp4"
-
-        #Hunt  Showdown 2019.07.03 - 21.54.55.07.DVR
-        file_edit = Process(target=clip_video(original_path, start_time, end_time, highlight_name))
+        file_edit = Process(target=clip_video(original_path, start_time, end_time, highlight_name, chosen_game))
         file_edit.start()
         file_edit.join()  # Waits until the clip_video function has finished executing
-        rename(original_path, ORIGINAL_DIRECTORY + original_name + "_EDITED" + ".mp4")
+        rename(original_path, BASE_DIRECTORY + GAME_LIST[chosen_game] + "\\" + original_name + "_EDITED" + ".mp4")
     except Exception as e: print(e) 
     
     finally:
-        input("Press enter to continue")
+        input("Video Complete! Press enter to continue")
